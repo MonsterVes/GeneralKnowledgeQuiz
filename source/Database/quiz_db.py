d6141ef77c19ed45d84afdc1dfb757c2a26779f6
MonsterVes <vszaprianova@gmail.com>
2025-04-30 09:44:55 +0300

    questions db

--- source/Database/quiz_db.py
+++ source/Database/quiz_db.py
@@ -1,8 +1,15 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
+import os
 
+# cwd = os.path()
+# print(cwd)
 
-engine = create_engine('sqlite:///quiz.db')
+db_folder = os.path.dirname(__file__)  # This gets the current file's directory
+db_path = os.path.join(db_folder, 'quiz.db')  # Create the full path to quiz.db
+
+# Create the SQLite engine with the correct path
+engine = create_engine(f'sqlite:///{db_path}')
 
 Base = declarative_base()
 
@@ -13,30 +20,43 @@ class QuestionDB(Base):
     question_text = Column(String, nullable = False)
     question_type = Column(String, nullable = False)
     category = Column(String, nullable = False)
+    true_false = relationship("TrueFalseQuestionDB", back_populates = "question", uselist = False)
+    multiple_choice = relationship("MultipleChoiceQuestionDB", back_populates = "question", uselist = False)
+    fill_in = relationship("FillInQuestionDB", back_populates = "question", uselist = False)
+    short_answer = relationship("ShortAnswerQuestionDB", back_populates = "question", uselist = False)
 
-class TrueFalseQuestionDB(QuestionDB):
-    __tablename__ = "t_f_questions" 
-    id = Column(Integer, ForeignKey("questions.id"), primary_key = True)
+class TrueFalseQuestionDB(Base):
+    __tablename__ = "true_false" 
+    id = Column(Integer, primary_key = True)
     answer = Column(String, nullable = False)
+    question_id = Column(Integer, ForeignKey("questions.id"), nullable = False)
+    question = relationship("QuestionDB", back_populates = "true_false")
 
-class MultipleChoiceQuestionDB(QuestionDB):
+class MultipleChoiceQuestionDB(Base):
     __tablename__ = "multiple_choice"
-    id = Column(Integer, ForeignKey("questions.id"), primary_key = True)
+    id = Column(Integer, primary_key = True)
     a = Column(String, nullable = False)
     b = Column(String, nullable = False)
     c = Column(String, nullable = False)
     d = Column(String, nullable = False)
     answer = Column(String, nullable = False)
+    question_id = Column(Integer, ForeignKey("questions.id"), nullable = False)
+    question = relationship("QuestionDB", back_populates = "multiple_choice")
 
-class FillInQuestionDB(QuestionDB):
+
+class FillInQuestionDB(Base):
     __tablename__ = "fill_in_questions"
-    id = Column(Integer, ForeignKey("questions.id"), primary_key = True)
+    id = Column(Integer,  primary_key = True)
     answer = Column(String, nullable = False)
+    question_id = Column(Integer, ForeignKey("questions.id"), nullable = False)
+    question = relationship("QuestionDB", back_populates = "fill_in")
 
-class ShortAnswerQuestionDB(QuestionDB):
+class ShortAnswerQuestionDB(Base):
     __tablename__ = "short_answer_questions"
-    id = Column(Integer, ForeignKey("questions.id"), primary_key = True)
+    id = Column(Integer, primary_key = True)
     answer = Column(String, nullable = False)
+    question_id = Column(Integer, ForeignKey("questions.id"), nullable = False)
+    question = relationship("QuestionDB", back_populates = "short_answer")
 
 
 Base.metadata.create_all(engine)
--- source/Quiz_Manager/question_manager.py
+++ source/Quiz_Manager/question_manager.py
@@ -11,84 +11,87 @@ class QuestionManager:
         try:
             category = input(
                 "Please enter category for the question\n"
-                " 1. General knowledge\n"
-                " 2. Natural sciences\n" 
+                " 1. General\n"
+                " 2. Science\n" 
                 " 3. Geography\n" 
-                "Your choice: ")
+                "Your choice (1, 2 or 3): ").strip()
+            if category not in ["1", "2", "3"]:
+                print("Invalid category choice. Please try again.")
+                return
+            
             question_type = input(
                 "What type of question would you like to add?\n"
                 " 1. True/False\n"
                 " 2. Multiple Choice\n" 
                 " 3. Fill in the blank\n" 
                 " 4. Short Answer\n"
-                "Your choice: ")
-            question_text = input("Please enter Question text: ").strip()
-            if not question_text:
+                "Your choice(1, 2, 3 or 4): ").strip()
+            if question_type not in ["1", "2", "3", "4"]:
+                print("Invalid question type choice. Please try again.")
+                return
+            new_question_text = input("Please enter Question text: ").strip()
+            if not new_question_text:
                 print("Question text cannot be empty")
                 return
+            new_question = db.QuestionDB(question_text = new_question_text, question_type = question_type, category = category)
+            
+            self.session.add(new_question)
+            self.session.commit()
+
+
             if question_type == "1":
-                answer = input("Please enter correct answer (True/False): ").strip()
+                answer = input("Please enter correct answer (True/False): ").strip().lower()
                 if answer not in ["true", "false"]:
                     print("Answer must be True or False")
                     return
-                question = db.TrueFalseQuestionDB(
-                    text=question_text, 
-                    answer=answer, 
-                    question_type = "TrueFalse", 
-                    category = category)
+                question = db.TrueFalseQuestionDB(answer=answer, question=new_question)
 
             elif question_type == "2":
                 options = []
                 for letter in ["A", "B", "C", "D"]:
-                    option = input(f"Oprion {letter}: ").strip()
+                    option = input(f"Option {letter}: ").strip()
                     if not option:
                         print("Option cannot be empty")
                         return
-                options.append(option)
+                    options.append(option)
 
-                answer = input("Please enter correct Option (A, B, C or D): ")
+                answer = input("Please enter correct Option (A, B, C or D): ").strip().lower()
                 if answer not in ["A", "B", "C", "D"]:
                     print("Answer must be A, B, C or D")
                     return
-                question = db.MultipleChoiceQuestionDB(
-                    text=question_text,                     
-                    a=options[0],
-                    b=options[1],
-                    c=options[2],
-                    d=options[3],
-                    answer=answer,
-                    question_type = "MultipleChice", 
-                    category = category
+                question = db.MultipleChoiceQuestionDB(                    
+                    a = options[0],
+                    b = options[1],
+                    c = options[2],
+                    d = options[3],
+                    answer = answer,
+                    question = new_question
                 )
             elif question_type == "3":
                 answer = input("Please enter correct answer: ")
                 if not answer:
                     print("Answer cannot be empty")
                     return
-                question = db.FillInQuestionDB(
-                    text=question_text, 
-                    answer=answer,
-                    question_type = "FillIn", 
-                    category = category)
+                question = db.FillInQuestionDB(answer = answer, question = new_question)
+            
             elif question_type == "4":
                 answer = input("Please enter correct answer: ")
                 if not answer:
                     print("Answer cannot be empty")
                     return
                 question = db.ShortAnswerQuestionDB(
-                    text=question_text, 
-                    answer=answer,
-                    question_type = "ShortAnswer", 
-                    category = category)
+                    answer = answer, question = new_question)
             else:
                 raise ValueError("Unsupported question type")
 
+            self.session.add(new_question)
             self.session.add(question)
             self.session.commit()
             print("Question added successfully!")
         except Exception as e:
             self.session.rollback()
-            print("Error adding question.")
+            print(f"Error adding question.{e}")
+            self.session.close()
         finally:
             self.session.close()
 
